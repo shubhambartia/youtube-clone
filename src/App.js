a424b376664276ebb2d64788021c464aa90f2266
shauydel <shubhambartia44@gmail.com>
2020-07-23 13:00:21 +0530

    youtube clone app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {Grid} from '@material-ui/core'
+import SearchBar from './components/SearchBar'
+import VideoDetail from './components/VideoDetail'
+import VideoList from './components/VideoList'
+import YouTube from './api/YouTube'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        }
+    }
+
+    componentDidMount() {
+        this.handleSubmit('BB ki vines')
+    }
+
+    handleSubmit = async(searchTerm) => {
+        const response = await YouTube.get('search' , {
+            params: {
+            part: 'snippet',
+            maxResults: 5,
+            key: 'AIzaSyAJBbZ2BjOj6DZBvpnHZ6RTyMYXoqgOTTo',
+            q: searchTerm,
+        }
+    })
+        //console.log(response.data.items);
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        })
+    }
+
+    onVideoSelect = (video) => {
+        this.setState({
+            selectedVideo:video
+        })
+    }
+
+    render() {
+        const {selectedVideo, videos} = this.state
+        return (
+            <Grid justify="center" container spacing={10}>
+                <Grid item xs={12}>
+                    <Grid container spacing={10}>
+                        <Grid item xs={12}>
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
+                        </Grid>
+
+                        <Grid item xs={8}>
+                            <VideoDetail video={selectedVideo}/>
+                        </Grid>
+                        <Grid item xs={4}>
+                            <VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
+        )
+    }
 }
-
-export default App;
+export default App
---
+++ src/components/VideoList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+
+import VideoItem from "./VideoItem";
+
+export default ({ videos, onVideoSelect }) => {
+  const listOfVideos = videos.map((video,id) => (
+    <VideoItem
+      onVideoSelect={onVideoSelect}
+      key={id}
+      video={video}
+    />
+  ));
+
+  return (
+    <Grid container spacing={10}>
+      {listOfVideos}
+    </Grid>
+  );
+}
